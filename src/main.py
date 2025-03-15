cc6cdd9c40873f7d12cffcde536ef8505ad35cfd
hamsar4j <hamsarajs8@gmail.com>
2025-03-15 23:39:52 +0800

    feat: added memory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
 from rag_workflow import build_rag_workflow
 
-if __name__ == "__main__":
+
+def main():
     rag_workflow = build_rag_workflow()
-    response = rag_workflow.invoke({"question": "What is the vision of SUTD?"})
-    print(response["answer"])
+    config = {"configurable": {"thread_id": "abc123"}}
+
+    print("RAG Workflow is ready!")
+    print("Type 'quit' or 'q' or 'exit' to stop the program.")
+    print("-------------------------------------------")
+
+    while True:
+        user_query = input("Enter your query: ")
+        if user_query.lower() in ["quit", "exit", "q"]:
+            break
+
+        try:
+            response = rag_workflow.invoke({"question": user_query}, config=config)
+            print(response["answer"])
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        print("-------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
--- src/rag_workflow.py
+++ src/rag_workflow.py
@@ -7,44 +7,51 @@ from langchain_core.prompts import ChatPromptTemplate
 from langgraph.checkpoint.memory import MemorySaver
 
 
-def build_rag_workflow():
-    # load llm
-    llm = init_chat_model(
-        model=config.llm_model,
-        api_key=config.groq_api_key,
-        model_provider=config.model_provider,
-    )
-    # load vector store
-    vector_store = VectorStore(config)
-    vector_store = vector_store.get_vector_store()
-    # load prompt
-    prompt = ChatPromptTemplate.from_template(
-        """
-        You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-        Question: {question} 
-        Context: {context} 
-        Answer:
-        """
-    )
+class RAGWorkflow:
+    def __init__(self):
+        self.config = config
+        self.vector_store = VectorStore(config).get_vector_store()
+        self.llm = init_chat_model(
+            model=config.llm_model,
+            api_key=config.groq_api_key,
+            model_provider=config.model_provider,
+        )
+        self.prompt = ChatPromptTemplate.from_template(
+            """
+            You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question}
+            Context: {context}
+            Answer:
+            """
+        )
 
-    def analyze_query(state: State):
-        structured_llm = llm.with_structured_output(Search)
+    def analyze_query(self, state: State):
+        structured_llm = self.llm.with_structured_output(Search)
         query = structured_llm.invoke(state["question"])
         return {"query": query}
 
-    def retrieve(state: State) -> State:
+    def retrieve(self, state: State) -> State:
         query = state["query"]
-        retrieved_docs = vector_store.similarity_search(query["query"])
+        retrieved_docs = self.vector_store.similarity_search(query["query"])
         return {"context": retrieved_docs}
 
-    def generate(state: State) -> State:
+    def generate(self, state: State) -> State:
         docs_content = "\n\n".join([doc.page_content for doc in state["context"]])
-        messages = prompt.invoke(
+        messages = self.prompt.invoke(
             {"question": state["question"], "context": docs_content}
         )
-        response = llm.invoke(messages)
+        response = self.llm.invoke(messages)
         return {"answer": response.content}
 
-    graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
-    graph_builder.add_edge(START, "analyze_query")
-    return graph_builder.compile()
+    def build(self):
+        graph_builder = StateGraph(State).add_sequence(
+            [self.analyze_query, self.retrieve, self.generate]
+        )
+        graph_builder.add_edge(START, "analyze_query")
+        memory = MemorySaver()
+        return graph_builder.compile(checkpointer=memory)
+
+
+def build_rag_workflow():
+    workflow = RAGWorkflow()
+    return workflow.build()
