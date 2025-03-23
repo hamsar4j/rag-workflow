936a8dcf8a29b521b5a240b1ce743a8dce35db4a
hamsar4j <hamsarajs8@gmail.com>
2025-03-23 15:16:37 +0800

    chore: upgraded embeddings model

--- app/config.py
+++ app/config.py
@@ -9,15 +9,15 @@ class Settings(BaseSettings):
     # qdrant vector db
     qdrant_url: str = os.getenv("QDRANT_URL")
     qdrant_api_key: str = os.getenv("QDRANT_API_KEY")
-    qdrant_collection_name: str = "test"
+    qdrant_collection_name: str = "sutd"
 
     # llm api
     groq_api_key: str = os.getenv("GROQ_API_KEY")
     llm_model: str = "groq:llama-3.3-70b-versatile"
 
     # ollama embeddings
-    embeddings_model: str = "nomic-embed-text"
-    embeddings_dim: int = 768
+    embeddings_model: str = "bge-m3"
+    embeddings_dim: int = 1024
 
 
 config = Settings()
